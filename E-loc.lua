47dceae06b7358e263925dd73b80f04bc73b22dd
koshiro564256-lgtm <koshiro564256@gmail.com>
2026-06-26 22:10:21 +0900

    Update E-loc.lua

--- E-loc.lua
+++ E-loc.lua
@@ -608,7 +608,7 @@ function OrionLib:MakeWindow(WindowConfig)
 		Position = UDim2.new(0.5, 0, 0, 0),
 		BackgroundTransparency = 1
 	}), {
-		AddThemeObject(SetProps(MakeElement("Image", "rbxassetid://7072725342"), {
+		AddThemeObject(SetProps(MakeElement("Image", "rbxassetid://90369054156879"), {
 			Position = UDim2.new(0, 9, 0, 6),
 			Size = UDim2.new(0, 18, 0, 18)
 		}), "Text")
